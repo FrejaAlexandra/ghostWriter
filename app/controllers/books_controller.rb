a82fad586fe312835de4c8cdd4ab59309c74dbdb
remcojonk <remcojonk@hotmail.com>
2022-11-29 12:59:55 +0100

    created books index, show, create pages

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,2 +1,44 @@
 class BooksController < ApplicationController
+
+  def index
+    @books = Book.all
+  end
+
+  def show
+    @book = Book.find(params[:id])
+  end
+
+  def new
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new(book_params)
+    @book.user = current_user
+    if @book.save
+      redirect_to book_path(@book)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+    @book.user = current_user
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    if @book = Book.update(book_params)
+      redirect_to book_path(@book)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :category, :description, :value, :shares, :photo, :example)
+  end
 end
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,5 @@
 class Book < ApplicationRecord
   belongs_to :user
+  validates :title, :description, :value, :shares, :category, :example, presence: true
+  validates :title, uniqueness: true
 end
