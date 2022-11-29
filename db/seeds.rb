cda2861044e5c6e7da36e86ba6f1eb47ded8fcbc
freya heaton <freyaheaton94@gmail.com>
2022-11-29 15:54:57 +0100

    make entire card a link

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,15 +6,15 @@
 #   books = Book.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-# puts "Cleaning database..."
-# Book.destroy_all
+puts "Cleaning database..."
+Book.destroy_all
 
-# puts "Creating restaurants..."
-# dishoom = {name: "Dishoom", address: "7 Boundary St, London E2 7JE", rating: 5}
-# pizza_east =  {name: "Pizza East", address: "56A Shoreditch High St, London E1 6PQ", rating: 4}
+puts "Creating restaurants..."
+dishoom = {name: "Dishoom", address: "7 Boundary St, London E2 7JE", rating: 5}
+pizza_east =  {name: "Pizza East", address: "56A Shoreditch High St, London E1 6PQ", rating: 4}
 
-# [dishoom, pizza_east].each do |attributes|
-#   restaurant = Restaurant.create!(attributes)
-#   puts "Created #{restaurant.name}"
-# end
-# puts "Finished!"
+[dishoom, pizza_east].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts "Finished!"
