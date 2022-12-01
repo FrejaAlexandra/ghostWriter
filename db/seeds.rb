89d59a93bdb0eaa646ca62f17c44ed3bda91d10e
remcojonk <remcojonk@hotmail.com>
2022-12-01 16:41:25 +0100

    changed shares to total_amount in books table

--- db/seeds.rb
+++ db/seeds.rb
@@ -43,14 +43,13 @@ book_cover = ["https://img.theculturetrip.com/450x/smart/images/56-3972722-51zvc
   book = Book.new(
     user_id: User.last.id,
     title: Faker::Book.title,
-
     psuedoname: Faker::Name.name_with_middle,
     description: Faker::Lorem.sentence(word_count: 500, supplemental: true),
     value: 45,
-    shares: 50,
+    total_amount: 50,
     example: Faker::Lorem.sentence(word_count: 1000, supplemental: true),
   )
   book.photo.attach(io: file, filename: "nes.png", content_type: "image/png")
-  book.save
+  book.save!
   puts book.title
 end
