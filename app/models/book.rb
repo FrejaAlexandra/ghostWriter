045eba5af0604acc03b7f594e5eae5b2ac621dc0
freya heaton <freyaheaton94@gmail.com>
2022-12-01 15:30:16 +0100

    our gorgeous database

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,7 +1,8 @@
 class Book < ApplicationRecord
   belongs_to :user
   validates :title, :description, :value, :shares, presence: true
-  # validates :title, uniqueness: true
+  
+  has_one_attached :photo
 
   acts_as_taggable_on :tags
 end
